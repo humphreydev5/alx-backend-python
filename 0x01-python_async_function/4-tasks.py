426e4553a3ecf37d38f25691ad2390e2a2cb9af1
humphreydev5 <humphreyikhalea^@gmail.com>
2024-07-08 07:54:06 +0100

    Task 4 - task_wait_n

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''Task 3's module.
+'''
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''Creates an asynchronous task for wait_random.
+    '''
+    return asyncio.create_task(wait_random(max_delay))
+
+
+
+#!/usr/bin/env python3
+'''Task 4's module.
+'''
+import asyncio
+from typing import List
