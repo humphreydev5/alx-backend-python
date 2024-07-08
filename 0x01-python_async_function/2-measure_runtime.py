0cb20c5957d6f2887a45dfc94c00cf7960eb97ec
humphreydev5 <humphreyikhalea^@gmail.com>
2024-07-08 07:51:08 +0100

    Task 3 - Function that takes integer

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-'''Task 3's module.
+'''Task 2's module.
 '''
 import asyncio
+import time
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task:
-    '''Creates an asynchronous task for wait_random.
+def measure_time(n: int, max_delay: int) -> float:
+    '''Computes the average runtime of wait_n.
     '''
-    return asyncio.create_task(wait_random(max_delay))
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.time() - start_time) / n
